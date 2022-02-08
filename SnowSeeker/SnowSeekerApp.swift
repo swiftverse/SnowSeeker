8c08c8acb1cce205ff71dfb2c9a11ae6935bd1c6
shivgirja <amitsrivastava115@yahoo.com>
2022-02-08 11:42:05 +0530

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Amit Shrivastava on 08/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
